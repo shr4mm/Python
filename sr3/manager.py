a1f9a60e5456f1ba62b4f609d9a009a7a3da21e2
Олександр <shr4mm@gmail.com>
2025-09-28 18:40:57 +0300

    updated class Manager added def bonus

--- sr3/manager.py
+++ sr3/manager.py
@@ -2,6 +2,7 @@ from employee import Employee
 
 
 class Manager(Employee):
+    premium=3000
     def __init__(self, name, salary, days_worked,bonus=0 ,number_of_subordinates=0):
         super().__init__(name,salary,days_worked,bonus)
         self._number_of_subordinates = number_of_subordinates
@@ -11,6 +12,9 @@ class Manager(Employee):
 
     def set_number_of_subordinates(self, number):
      self._number_of_subordinates = number
+
+    def bonus(self):
+        return super().bonus()+ (self._number_of_subordinates * Manager.premium)
 
     def report(self):
         return f"Менеджер {self._name} керує {self._number_of_subordinates} співробітниками."
