db3ae8f65d5f0ed29d1a7347d6a80ebb7f5e155b
Олександр <shr4mm@gmail.com>
2025-09-28 18:39:11 +0300

    updated class Employee ,attributes protected and added getter and setter

--- sr3/employee.py
+++ sr3/employee.py
@@ -1,12 +1,28 @@
 class Employee:
     def __init__(self, name, salary, days_worked,bonus=0):
-        self.name = name
-        self.salary = salary
-        self.days_worked = days_worked
-        self.bonus = bonus
+        self._name = name
+        self._salary = salary
+        self._days_worked = days_worked
+        self._bonus = bonus
+    def get_name(self):
+        return self._name
+    def get_salary(self):
+        return self._salary
+    def get_days_worked(self):
+        return self._days_worked
+    def get_bonus(self):
+        return self._bonus
+    def set_bonus(self, bonus):
+        self._bonus = bonus
+    def set_salary(self, salary):
+        self._salary = salary
+    def set_days_worked(self, days_worked):
+        self._days_worked = days_worked
+    def set_name(self, name):
+        self._name = name
 
     def calculate_monthly_salary(self):
-       return (self.salary/ 30) * self.days_worked
+       return (self._salary/ 30) * self._days_worked
 
     def bonus(self):
-        return (self.calculate_monthly_salary() / 100) * self.bonus
+        return (self.calculate_monthly_salary() / 100) * self._bonus
